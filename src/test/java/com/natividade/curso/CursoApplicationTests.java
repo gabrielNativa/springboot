3b937393580156bb91bee80adc7156d71e7e4ec5
gabrielNativa <gabrielmoreira450@outlook.com>
2025-09-29 14:04:46 -0300

    Projeto criado

---
+++ src/test/java/com/natividade/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natividade.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
