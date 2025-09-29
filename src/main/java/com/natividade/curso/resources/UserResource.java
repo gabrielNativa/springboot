c3a8fd1d915aef8e051b4dba7d627939cda655a7
gabrielNativa <gabrielmoreira450@outlook.com>
2025-09-29 14:29:48 -0300

    Entidade usuario

---
+++ src/main/java/com/natividade/curso/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.natividade.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.natividade.curso.entities.User;
+
+@RestController 
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Mara", "mra@gmail.com", "9999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+}
