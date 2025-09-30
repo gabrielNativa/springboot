13b918de0c0ece3fc6002ba793e78b30adcf5f68
gabrielNativa <gabrielmoreira450@outlook.com>
2025-09-30 15:28:40 -0300

    Parte 2 order

---
+++ src/main/java/com/natividade/curso/services/OrderService.java
@@ -0,0 +1,30 @@
+package com.natividade.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.natividade.curso.entities.Order;
+
+import com.natividade.curso.repositories.OrderRepository;
+
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional <Order> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
