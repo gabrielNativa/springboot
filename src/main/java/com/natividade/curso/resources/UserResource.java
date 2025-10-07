b4c9a07a8cfcc79aa86c097e4a1a6d3131debd36
gabrielNativa <gabrielmoreira450@outlook.com>
2025-10-07 15:35:42 -0300

    User insert

--- src/main/java/com/natividade/curso/resources/UserResource.java
+++ src/main/java/com/natividade/curso/resources/UserResource.java
@@ -1,18 +1,24 @@
 package com.natividade.curso.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.natividade.curso.entities.User;
 import com.natividade.curso.services.UserService;
 
-@RestController 
+
+
+@RestController // converte para metodo json
 @RequestMapping(value = "/users")
 public class UserResource {
 	
@@ -30,5 +36,13 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
 	
 }
