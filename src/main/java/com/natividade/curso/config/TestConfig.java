da7de7c13d3377f89fac8c813086206f8cfaed1f
gabrielNativa <gabrielmoreira450@outlook.com>
2025-10-07 15:08:45 -0300

    Payment One to One association

--- src/main/java/com/natividade/curso/config/TestConfig.java
+++ src/main/java/com/natividade/curso/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.natividade.curso.entities.Category;
 import com.natividade.curso.entities.OrderItem;
+import com.natividade.curso.entities.Payment;
 import com.natividade.curso.entities.Order;
 
 import com.natividade.curso.entities.Product;
@@ -86,6 +87,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 
 	}
 }
